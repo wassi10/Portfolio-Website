4eaadb8fe1041c35ab340ca5021918075fe92d66
wassi10 <khadizawassi@gmail.com>
2023-06-24 18:44:10 +0600

    Responsive ongoing

--- js/script.js
+++ js/script.js
@@ -24,6 +24,12 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*='+id+']').classList.add('active');
             });
+            // Active Sections for Animation on scroll
+            sec.classList.add('show-animate');
+        }
+        // if want to use animation that repeats on scroll , use this
+        else{
+            sec.classList.remove('show-animate');
         }
     });
 
@@ -32,7 +38,8 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.scrollY > 100);
 
-    // remove toggole icon and navbar when click navbar links (scroll)
+    // remove toggle icon and navbar when click navbar links (scroll)
+
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
