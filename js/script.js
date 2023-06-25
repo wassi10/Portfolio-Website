976f531d9c4250ffa73625cc69e169687b4625a1
wassi10 <khadizawassi@gmail.com>
2023-06-25 19:55:08 +0600

    Animation Ongoing

--- js/script.js
+++ js/script.js
@@ -42,4 +42,9 @@ window.onscroll = () => {
 
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
+
+    // Animation Footer on Scroll
+
+    let footer = document.querySelector('footer');
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
